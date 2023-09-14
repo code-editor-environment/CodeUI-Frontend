93fd5247c0de5d49093943c624248e5e7103f71a
tranquoclong <tranquoclong1310@gmail.com>
2023-09-14 17:27:54 +0700

    up

--- src/store/constants/user.const.jsx
+++
@@ -1,5 +0,0 @@
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_FAILED = "LOGIN_FAILED";
-export const ACT_LOGOUT = "ACT_LOGOUT";
-export const USER_SUCCESS = "USER_SUCCESS";
-export const USER_FAILED = "USER_FAILED";
